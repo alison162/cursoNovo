79211f0fc00337b4f5e95affe3cf6227e110912f
alison162 <porcos2017@gmail.com>
2026-06-05 09:59:27 -0300

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,20 +7,33 @@
     <title>Sistema Curso</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
 </head>
 
 <body>
+    <style>
+        .produto-img {
+            width: 300px;
+            height: 400px;
+            object-fit: cover;
+        }
+    </style>
     <section id="topo" class="container">
         <div class="row">
-            <div class="col-12" style="background-color:#3FA14C; height: 100px; padding-top: 40px; color:#ffffff">
-                <h3> Cursos - 32218 </h3>
+            <div class="col-12 d-flex align-items-center"
+                style="background-color:#6a842c; height: 100px; color:#ffffff">
+
+                <img src="{{ asset('images/logo_curso.png') }}" alt="Logo"
+                    style="width:150px; height:150px; object-fit:contain; margin-right:15px;">
+
+                <h3 class="m-0">Cursos - 32218</h3>
             </div>
         </div>
     </section>
     <section id="menu" class="container">
         <div class="row">
 
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <nav class="navbar navbar-expand-lg bg-success-subtle">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="#"></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
@@ -39,25 +52,50 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('cadastrarMatricula') }}">Matricula</a>
                             </li>
-
-
                         </ul>
                     </div>
                 </div>
             </nav>
-
         </div>
     </section>
+
     <div id="conteudo" class="container">
         <div class="row">
             <div class="offset-2 col-8">
                 <div class="card">
-                    <div class="card-header" style="background-color:#215428;color:#ffffff">
-                        Cadastrar Alunos
+                    <div class="card-header" style="background-color:#6a842c;color:#ffffff">
+                        Sistema de Cursos
                     </div>
-                    <div class="card-body">
-            <h1>Bem Vindos ao Sistema de Cursos</h1>
-
+                    <div id="carouselExampleIndicators" class="carousel slide">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
+                                class="active" aria-current="true" aria-label="img1"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                                aria-label="img2"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                                aria-label="img3"></button>
+                        </div>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="images/img1.jpg" class="d-block w-100 produto-img" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="images/img2.jpg" class="d-block w-100 produto-img" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="images/img3.jpg" class="d-block w-100 produto-img" alt="...">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                            data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                            data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
                     </div>
                 </div>
             </div>
@@ -66,7 +104,7 @@
         </section>
         <section id="rodape" class="container">
             <div class="row">
-                <div class="col-12 mt-4" style="background-color:#3FA14C; height: 70px;">
+                <div class="col-12 mt-4" style="background-color:#6a842c; height: 70px;">
                 </div>
             </div>
         </section>
